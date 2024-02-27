7c050bea4cfdc54c4e995edf500505b6b3d0a36a
Zhang Xiaoyu <104520080+Gurdiis@users.noreply.github.com>
2024-02-27 13:55:57 +0900

    scan or input number to show details

---
+++ vegan japan app for iphone/vegan japan app for iphone/Models/ProductService.swift
@@ -0,0 +1,8 @@
+//
+//  ProductService.swift
+//  vegan japan app for iphone
+//
+//  Created by XIAOYU ZHANG on 2024/02/25.
+//
+
+import Foundation
---
+++ vegan japan app for iphone/vegan japan app for iphone/Views/BarcodeScannerViewController.swift
@@ -0,0 +1,8 @@
+//
+//  BarcodeScannerViewController.swift
+//  vegan japan app for iphone
+//
+//  Created by XIAOYU ZHANG on 2024/02/26.
+//
+
+import Foundation
